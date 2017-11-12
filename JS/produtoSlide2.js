a57c8e5f74e1f5b50324338d48411584e646aeab
GouTReX <leoadan02@gmail.com>
2017-11-12 15:09:47 -0200

    Produtos na home e Pagina de Produto

---
+++ JS/produtoSlide2.js
@@ -0,0 +1,42 @@
+$(document).ready(
+    function(){
+        var produto = new Array();
+        var legenda = new Array();
+        var cont = 0;
+        
+        produto[0]= "img/produtos/ProcessadorqRyzen1800X/box.jpg";  
+        
+        produto[1]= "img/produtos/placamae/amd/MSI-X370-XPower-Gaming-Titanium/cima.jpg";
+        
+        produto[2]= "img/produtos/placamae/amd/MSI-X370-XPower-Gaming-Titanium/conectores.jpg";
+        
+
+        carregaDestaque(0);
+        
+        function carregaDestaque(idx){
+            $("#imgGde").hide().attr("src",produto[idx]).fadeIn();
+            var aux = produto[idx].split("/");
+            var nomeProduto = aux[1].split(".");
+        }
+        
+        $("#arrowleftProduto").click(
+            function() {
+                cont = cont-1;
+                if(cont==-1) {
+                    cont = produto.length-1;
+                }
+                 carregaDestaque(cont);            
+            }
+        );
+        
+        $("#arrowrightProduto").click(
+            function(){
+                cont = cont+1;
+                if(cont==produto.length) {
+                    cont = 0;
+                }
+                 carregaDestaque(cont);            
+            }
+        );
+    }
+)
